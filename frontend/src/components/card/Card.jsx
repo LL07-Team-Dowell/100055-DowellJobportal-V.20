6eca4a19e48169ea8caf65c9fcf8792b664b9d36
Abanoub Milad <59348949+Abanoub-pixel@users.noreply.github.com>
2022-12-11 00:21:04 +0200

    new changes

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -70,7 +70,7 @@ const Card = () => {
               backgroundColor: "#7C7C7C",
             }}
           ></span>
-          <div style={{ display: "flex", gap: "50px" }}>
+          <div style={{ display: "flex", gap: "50px", paddingBottom: "19px" }}>
             Stipend: 15000 Rs
             <button className={style.btngreen}>Full time</button>
           </div>
